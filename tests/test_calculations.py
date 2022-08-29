b2851cd674fa8ee39319c4243d0c0ae16ce74199
Boscosylvester John Chittilapilly <bchitti@ncsu.edu>
2022-08-28 20:27:47 -0400

    Updated test cases

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,18 +1,21 @@
-import pytest
 from src import add, subtract, multiply, divide
 
+
 def test_add():
-    assert add(1,2) == 3
-    assert add(1,-2) == -1
+    assert add(1, 2) == 3
+    assert add(1, -2) == -1
+
 
 def test_subtract():
-    assert subtract(1,2) == -1
-    assert subtract(1,-2) == 3
+    assert subtract(1, 2) == -1
+    assert subtract(1, -2) == 3
+
 
 def test_multiply():
-    assert multiply(1,2) == 2
-    assert multiply(1,-2) == -2
+    assert multiply(1, 2) == 2
+    assert multiply(1, -2) == -2
+
 
 def test_divide():
-    assert divide(1,2) == 0.5
-    assert divide(1,-2) == -0.5
+    assert divide(1, 2) == 0.5
+    assert divide(1, -2) == -0.5
